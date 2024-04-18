ce4a0d9ef0ed191e8b098c646763cc4e11f282af
luca <soyluca>
2024-04-17 21:18:05 -0300

    new commit

---
+++ Personal_Proyects/Python/Other proyects/goggle_searching.py
@@ -0,0 +1,33 @@
+#webbrowser
+
+import webbrowser as wb
+
+def func():
+    
+    class Class():
+        
+        def __init_(self):
+            pass
+        
+        def define_search(self):
+            
+            srch = input("que queres buscar: ")
+            
+            wb.open(f"https://www.google.com/search?q={srch}+&oq={srch}+&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQLhhA0gEIMjg3OWowajGoAgCwAgA&sourceid=chrome&ie=UTF-8")
+            
+            print("Page opened")
+    
+    instance = Class()
+    
+    ask = " "
+    
+    while ask not in "no":
+        
+        instance.define_search()
+        ask = input("Do you want to continue searching: ")
+
+func()
+        
+        
+#https://www.google.com/search?q=search+&oq=search+&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQLhhA0gEIMjg3OWowajGoAgCwAgA&sourceid=chrome&ie=UTF-8
+
